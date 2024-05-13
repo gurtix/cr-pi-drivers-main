7aa92fd53a59be76544b50f197805cbf2e54fd36
Gurtix <gurtix@hotmail.com>
2024-05-13 11:56:26 -0500

    añadi una vista previa a las imagenes en el formulario

--- client/src/create/Create.jsx
+++ client/src/create/Create.jsx
@@ -1,9 +1,9 @@
 import "./Create.css";
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useParams, Link  } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 
-function Create (){
+function Create() {
     const [description, setDescription] = useState('');
     const [image, setImage] = useState('');
     const [nationality, setNationality] = useState('');
@@ -37,26 +37,26 @@ function Create (){
             setErrorMessage('Todos los campos son obligatorios');
             return;
         }
-    const driverData = { 
-        name: { forename, surname }, 
-        description, 
-        image: { url: image }, 
-        nationality, 
-        dob, 
-        teams 
-    };
+        const driverData = {
+            name: { forename, surname },
+            description,
+            image: { url: image },
+            nationality,
+            dob,
+            teams
+        };
         try {
             const response = await axios.post('http://localhost:3001/Drivers', driverData);
             console.log(response.data);
-            setSuccessMessage('CORREDOR CREADO!');
+            setSuccessMessage('CREACIÓN EXITOSA');
             setErrorMessage('');
         } catch (error) {
             console.error(error);
             setErrorMessage('ERROR AL CREAR CORREDOR');
         }
-    };    
+    };
 
-    return(
+    return (
         <div className="create">
             <header className={`header ${isScrolled ? 'hidden' : ''}`}>
                 <Link to="/home" className="boton">
@@ -65,12 +65,12 @@ function Create (){
             </header>
             <div className="separador"></div>
             <div className="tittle">
-            <h3>CREAR CORREDOR</h3>
+                <h3>CREAR CORREDOR</h3>
             </div>
             <form onSubmit={handleSubmit}>
-            <label>
-                <h2>Nombre:</h2>
-                    <input type="text" value={forename} onChange={(e) => setForename(e.target.value)} className="data"/>
+                <label>
+                    <h2>Nombre:</h2>
+                    <input type="text" value={forename} onChange={(e) => setForename(e.target.value)} className="data" />
                 </label>
                 <label>
                     <h2>Apellido:</h2>
@@ -78,32 +78,33 @@ function Create (){
                 </label>
                 <label>
                     <h2>Fecha de nacimiento:</h2>
-                    <input type="date" value={dob} onChange={(e) => setDob(e.target.value)} className="dob-input"/>
+                    <input type="date" value={dob} onChange={(e) => setDob(e.target.value)} className="dob-input" />
                 </label>
                 <label>
-                    <h2>Imagen:</h2>                    
+                    <h2>Imagen:</h2>
                     <input type="text" value={image} onChange={(e) => setImage(e.target.value)} className="data" />
+                    {image && <img src={image} alt="Vista previa" style={{ width: '100px', height: '100px' }} />}
                 </label>
                 <label>
                     <h2>Nacionalidad:</h2>
                     <input type="text" value={nationality} onChange={(e) => setNationality(e.target.value)} className="data" />
                 </label>
-                <label>                    
-                <h2>Escuderías:</h2>                    
+                <label>
+                    <h2>Escuderías:</h2>
                     <input type="text" value={teams} onChange={(e) => setTeams(e.target.value)} className="data" />
                 </label>
                 <label>
-                    <h2>Descripción:</h2>                    
+                    <h2>Descripción:</h2>
                     <textarea value={description} onChange={(e) => setDescription(e.target.value)} className="description-input" />
-                </label>                
+                </label>
                 <input type="submit" value="Crear Corredor" className="boton-crear" />
-            <div className="error-message">
-            {successMessage && <p className='mensajeCreado'>{successMessage}</p>}
-            {errorMessage && <p className='mensajeError'>{errorMessage}</p>}
-            </div>
+                <div className="error-message">
+                    {successMessage && <p className='mensajeCreado'>{successMessage}</p>}
+                    {errorMessage && <p className='mensajeError'>{errorMessage}</p>}
+                </div>
             </form>
             <div className="bottom"></div>
-            
+
         </div>
     )
 }
